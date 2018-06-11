4e30b83a4cc25a1803d5ab69df8aadb64cbef41f
ilyassR <ilyass.rahmoune@gmail.com>
2018-06-11 16:20:49 +0200

    Update Login Template

--- src/app/mock-resultats.ts
+++ src/app/mock-resultats.ts
@@ -1,5 +1,6 @@
 import { Resultat } from './resultat';
 
 export const RESULTATS: Resultat[] = [
-    {id:1, candidat: 'candidat1', date: new Date('2018-02-12T00:00:00') , niveau:'Moyen', nomTest:'Java', noteFinale: 20 , correction:'corre'}
+    {id:1, candidat: 'candidat1', date: new Date('2018-02-12T00:00:00') , niveau:'Moyen', nomTest:'Java', noteFinale: 20 , correction:'corre'},
+    {id:2, candidat: 'candidat2', date: new Date('2018-02-12T00:00:00') , niveau:'Moyen', nomTest:'Java', noteFinale: 19 , correction:'corre'}
 ]; 
