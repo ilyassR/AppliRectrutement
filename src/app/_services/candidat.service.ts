aa8e152b3a2b961e9366f2a855775b787c7a1123
ilyassR <ilyass.rahmoune@gmail.com>
2018-06-13 14:24:08 +0200

    angular-cli server : Proxy API requests to the server backend
    Call of web service to get List of All Candidats

--- src/app/_services/candidat.service.ts
+++ src/app/_services/candidat.service.ts
@@ -3,17 +3,21 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import {Observable} from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { map } from 'rxjs/operators';
 
 import { Candidat } from '../_models/candidat';
 import { CANDIDATS } from '../mock-candidats';
 
 @Injectable()
 export class CandidatService {
-
+  private userUrl = '/api';
   constructor(private http: HttpClient) { }
-
+/*
   getCandidats(): Observable<Candidat[]> {
         return of(CANDIDATS);
+  }*/
+  getCandidats(): Observable<Candidat[]> {
+      return this.http.get<Candidat[]>( this.userUrl + '/candidats');
   }
 
 }
